a91f3404dff57c78b2dbaa20efeb85802b1e53b4
rammyyadav-dot <rammy.yadav@gmail.com>
2026-09-26 22:38:06 +0530

    feat(agent): register search cache port

--- apps/api/src/agent/agent.module.ts
+++ apps/api/src/agent/agent.module.ts
@@ -9,6 +9,8 @@ import { TenantContextGuard } from './tenant-context.guard'
 import { SUPPLIER_ADAPTER, UnconfiguredSupplierAdapter } from './supplier.port'
 import { InventoryHoldService } from './inventory-hold.service'
 import { OfferHoldService } from './offer-hold.service'
+import { AgentSearchService } from './agent-search.service'
+import { CACHE_PORT, NoopCache } from '../common/cache/cache.port'
 
 @Module({
   imports: [AuthModule],
@@ -21,6 +23,8 @@ import { OfferHoldService } from './offer-hold.service'
     AgentFinanceService,
     InventoryHoldService,
     OfferHoldService,
+    AgentSearchService,
+    { provide: CACHE_PORT, useClass: NoopCache },
     { provide: SUPPLIER_ADAPTER, useClass: UnconfiguredSupplierAdapter },
   ],
   exports: [AgentRbacGuard, TenantContextGuard, SUPPLIER_ADAPTER],
